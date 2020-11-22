f1af540aa88f0ff6436782dd5efd1aa94baa86fd
Eren Yildirir <eren.yildirir@gmail.com>
2020-11-22 02:32:07 -0500

    Switched from Pitch/Heading to Direction vector attitude.

--- src/testbed_nano_hover.cpp
+++ src/testbed_nano_hover.cpp
@@ -5,9 +5,12 @@
 #include <krpc.hpp>
 #include <krpc/services/space_center.hpp>
 #include <unistd.h>
+#include <boost/geometry.hpp>
 
 #define PI 3.14159265
 #define HOST_IP "192.168.1.78"
+
+namespace bg = boost::geometry;
 
 int main()
 {
@@ -44,15 +47,14 @@ int main()
     float target_msl = 200;
     float target_agl = 20;
     float target_altitude;
-    float current_altitude;
     float target_pitch = 90;
     float target_heading = 0;
-    float target_roll = 0;
-    std::tuple<double, double, double> vcart;
     // float lat_init = lat();
     // float lon_init = lon();
-    float lat_init = -0.0967313;
-    float lon_init = -74.6184;
+
+    float lat_init = -0.0968136;
+    float lon_init = -74.6174;
+
     float lat_error, lon_error;
     float target_vlat, target_vlon;
     float vlat_error, vlon_error;
@@ -82,22 +84,36 @@ int main()
     start = std::clock();
     bool stopSet = false;
     bool arrived = false;
-    
-    while (fuel() > 0.01 * max_fuel)
+    int consoleCntr = 0;
+
+    float current_agl = agl();
+    float current_msl = msl();
+    float current_altitude;
+    float current_thrust_avail = thrust_avail();
+    float current_mass = mass();
+    auto current_situation = vessel.situation();
+    float current_lat = lat();
+    float current_lon = lon();
+    float current_vz = vz();
+    std::tuple<double, double, double> vcart = vtuple();
+    std::tuple<double, double, double> target_dir;
+    float current_fuel = fuel();
+
+    while (current_fuel > 0.01 * max_fuel)
     {
-        if (agl() < target_agl && msl() >= target_msl - 3)
+        if (current_agl < target_agl && current_msl >= target_msl - 3)
         {
             target_altitude = target_agl;
-            current_altitude = agl();
+            current_altitude = current_agl;
         }
         else
         {
             target_altitude = target_msl;
-            current_altitude = msl();
+            current_altitude = current_msl;
         }
 
-        twr = thrust_avail() / (mass() * g);
-        if (fuel() > 0.25 * max_fuel && !arrived)
+        twr = current_thrust_avail / (current_mass * g);
+        if (current_fuel > 0.25 * max_fuel && !arrived)
         {
             alt_error = current_altitude - target_altitude;
             target_vz = -0.5 * alt_error;
@@ -106,9 +122,8 @@ int main()
             else if (target_vz < -4)
                 target_vz = -4;
         }
-        else if (vessel.situation() == krpc::services::SpaceCenter::VesselSituation::landed)
+        else if (current_situation == krpc::services::SpaceCenter::VesselSituation::landed)
         {
-            vessel.control().set_throttle(0);
             break;
         }
         else if (arrived)
@@ -120,24 +135,20 @@ int main()
             target_vz = -4;
             if (!stopSet)
             {
-                lat_init = lat();
-                lon_init = lon();
+                lat_init = current_lat;
+                lon_init = current_lon;
                 stopSet = true;
             }
         }
-        vz_error = vz() - target_vz;
+        vz_error = current_vz - target_vz;
         thrust = (0.5 * -vz_error + 1) * (1 / twr);
         if (thrust > 1)
             thrust = 1;
         else if (thrust < 0)
             thrust = 0;
 
-        vessel.control().set_throttle(thrust);
-
-        vcart = vtuple();
-
-        lat_error = lat() - lat_init;
-        lon_error = lon() - lon_init;
+        lat_error = current_lat - lat_init;
+        lon_error = current_lon - lon_init;
 
         if (sqrt(pow(lat_error, 2) + pow(lon_error, 2)) < 0.0004 && sqrt(pow(std::get<1>(vcart), 2) + pow(std::get<2>(vcart), 2)) < 0.2)
             arrived = true;
@@ -152,27 +163,14 @@ int main()
             target_vlon = max_vh * target_vlon / target_vh;
         }
 
-        // if (target_vlat > 10)
-        //     target_vlat = 10;
-        // if (target_vlat < -10)
-        //     target_vlat = -10;
-        // if (target_vlon > 10)
-        //     target_vlon = 10;
-        // if (target_vlon < -10)
-        //     target_vlon = -10;
-
         vlat_error = std::get<1>(vcart) - target_vlat;
         vlon_error = std::get<2>(vcart) - target_vlon;
 
-        target_pitch = 90 - 0.7 * (pow(vlat_error, 2) + pow(vlon_error, 2));
+        target_pitch = 90 - 1.5 * sqrt(pow(vlat_error, 2) + pow(vlon_error, 2));
         target_heading = atan2(vlon_error, vlat_error) / PI * 180 + 180;
-        // target_roll = target_heading - 360;
 
-        // target_pitch = 90 - vh();
-        // target_heading = atan2(std::get<0>(vcart), std::get<1>(vcart)) / PI * 180 + 180;
-
-        if (target_pitch < 80)
-            target_pitch = 80;
+        if (target_pitch < 75)
+            target_pitch = 75;
         else if (target_pitch > 90)
         {
             target_pitch = 180 - target_pitch;
@@ -183,22 +181,49 @@ int main()
         else if (target_heading > 360)
             target_heading -= 360;
 
-        ap.target_pitch_and_heading(target_pitch, target_heading);
-        // ap.set_target_roll(target_roll);
+        bg::model::point<long double, 2, bg::cs::spherical<bg::degree>> dir_spherical(90 - target_heading, 90 - target_pitch);
+        bg::model::point<long double, 3, bg::cs::cartesian> dir_cartesian;
+        bg::transform(dir_spherical, dir_cartesian);
+        target_dir = std::make_tuple(bg::get<2>(dir_cartesian), bg::get<1>(dir_cartesian), bg::get<0>(dir_cartesian));
 
-        std::cout << "Report:" << std::endl;
-        std::cout << "lat_error: " << lat_error << ", ";
-        std::cout << "lon_error: " << lon_error << ", ";
-        std::cout << "vlat: " << std::get<1>(vcart) << ", ";
-        std::cout << "target_vlat: " << target_vlat << ", ";
-        std::cout << "vlat_error: " << vlat_error << ", ";
-        std::cout << "vlon: " << std::get<2>(vcart) << ", ";
-        std::cout << "target_vlon: " << target_vlon << ", ";
-        std::cout << "vlon_error: " << vlon_error << ", ";
-        std::cout << "Fuel Ratio: " << fuel() / max_fuel << std::endl
-                  << std::endl;
-        usleep(30);
+        vessel.control().set_throttle(thrust);
+        // ap.target_pitch_and_heading(target_pitch, target_heading);
+        // ap.set_target_roll(target_roll);
+        ap.set_target_direction(target_dir);
+
+        if (consoleCntr < 20)
+        {
+            std::cout << "Report:" << std::endl;
+            std::cout << "lat_error: " << lat_error << ", ";
+            std::cout << "lon_error: " << lon_error << ", ";
+            std::cout << "vlat: " << std::get<1>(vcart) << ", ";
+            std::cout << "target_vlat: " << target_vlat << ", ";
+            std::cout << "vlat_error: " << vlat_error << ", ";
+            std::cout << "vlon: " << std::get<2>(vcart) << ", ";
+            std::cout << "target_vlon: " << target_vlon << ", ";
+            std::cout << "vlon_error: " << vlon_error << ", ";
+            std::cout << "Fuel Ratio: " << fuel() / max_fuel << std::endl
+                      << std::endl;
+            consoleCntr = 0;
+        }
+        else
+            consoleCntr++;
+
+        usleep(50);
+
+        current_agl = agl();
+        current_msl = msl();
+        current_altitude;
+        current_thrust_avail = thrust_avail();
+        current_mass = mass();
+        current_situation = vessel.situation();
+        current_lat = lat();
+        current_lon = lon();
+        current_vz = vz();
+        vcart = vtuple();
+        current_fuel = fuel();
     }
+    vessel.control().set_throttle(0);
     ap.disengage();
     vessel.control().set_sas(true);
 }
